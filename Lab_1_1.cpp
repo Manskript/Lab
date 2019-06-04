0683c9f616e05f1dbb6999fe0d768db92a6adaea
Manskript <49438600+Manskript@users.noreply.github.com>
2019-06-04 10:12:33 +0300

    Add files via upload

---
+++ Lab_1_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "Complex.h"
+using namespace std;
+
+int main() {
+	double re, im;
+
+	cout << "Enter Re(a): ";
+	cin >> re;
+	cout << "Enter Im(a): ";
+	cin >> im;
+	Complex c1(re, im);
+
+	cout << "Enter Re(b): ";
+	cin >> re;
+	cout << "Enter Im(b): ";
+	cin >> im;
+	Complex c2(re, im);
+
+	cout << "a + b = " << c1 + c2 << endl;
+	cout << "a - b = " << c1 - c2 << endl;
+	cout << "a * b = " << c1 * c2 << endl;
+	cout << "a / b = " << c1 / c2 << endl;
+
+	system("pause");
+	return 0;
+}
